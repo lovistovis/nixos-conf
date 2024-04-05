bc4cb2092d0b53a92598aadff92af663922ad594
Love Lysell Berglund <lovistovis0@gmail.com>
2024-04-06 00:07:53 +0200

    huy

--- nixos/global.nix
+++ nixos/global.nix
@@ -12,6 +12,8 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  # security.polkit.enable = true;
+
   # Select internationalisation properties.
   i18n = {
     defaultLocale = "en_US.UTF-8";
