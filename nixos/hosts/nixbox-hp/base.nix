b06a885bf6e2c70476c9fbf6c961c01732f35e09
Love Lysell Berglund <lovistovis0@gmail.com>
2024-04-05 19:39:33 +0200

    heyo

--- nixos/hosts/nixbox-hp/base.nix
+++ nixos/hosts/nixbox-hp/base.nix
@@ -27,5 +27,27 @@
     };
   };
 
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+# services.xserver.videoDrivers = [ "nvidia" ];
+
+# hardware.nvidia = {
+  # modesetting.enable = true;
+
+  # powerManagment.enable = false;
+
+  # powerManagement.finegrained = false;
+
+  # open = false;
+
+  # nvidiaSettings = true;
+
+  #  package = config.boot.kernelPackages.nvidiaPackages.stable;
+  #};
+
   time.hardwareClockInLocalTime = true;
 }
--- nixos/hosts/nixbox-hp/desktop.nix
+++ nixos/hosts/nixbox-hp/desktop.nix
@@ -1,4 +1,9 @@
 { pkgs, ... }:
 {
+  programs = {
+  };
 
+  home.packages = with pkgs; [
+    lshw
+  ];
 }
