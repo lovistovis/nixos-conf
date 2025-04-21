12503a035b7516da0aef7697959875d7ca2de95f
Love Lysell Berglund <lovistovis0@gmail.com>
2025-04-21 17:50:07 +0200

    power save

--- hosts/nixbox-hp/base.nix
+++ hosts/nixbox-hp/base.nix
@@ -38,6 +38,18 @@
     };
   };
 
+  services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.settings = {
+    battery = {
+       governor = "powersave";
+       turbo = "never";
+    };
+    charger = {
+       governor = "performance";
+       turbo = "auto";
+    };
+  };
+
   hardware = {
     opengl = {
       enable = true;
