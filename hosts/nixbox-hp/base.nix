c9f429c13e4178810edb242a2802186840181f1b
Love Lysell Berglund <lovistovis0@gmail.com>
2025-04-25 11:32:37 +0200

    changes

--- hosts/nixbox-hp/base.nix
+++ hosts/nixbox-hp/base.nix
@@ -94,25 +94,6 @@
         Option "SwapbuffersWait" "true"
         Option "DPI" "2"
       '';
-      # config = ''
-      #   Section "Device"
-      #     Identifier  "Intel Graphics"
-      #     Driver      "intel"
-      #     BusID       "PCI:0:2:0"
-      #   EndSection
-
-      #   Section "Device"
-      #     Identifier  "nvidia"
-      #     Driver      "nvidia"
-      #     BusID       "PCI:1:0:0"
-      #     Option      "AllowEmptyInitialConfiguration"
-      #   EndSection
-      # '';
-      # screenSection = ''
-      #   Option         "metamodes" "nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
-      #   Option         "AllowIndirectGLXProtocol" "off"
-      #   Option         "TripleBuffer" "on"
-      # '';
       dpi = 80;
     };
     thermald.enable = true;
