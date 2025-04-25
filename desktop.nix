d80c76a85cf80d37331d3276ad3fc4162c40d280
Love Lysell Berglund <lovistovis0@gmail.com>
2025-04-25 22:09:25 +0200

    brightness contol fix

--- desktop.nix
+++ desktop.nix
@@ -349,8 +349,8 @@ in {
         bindsym ${mod}+Shift+s exec 'grim -g "$(slurp)" - | wl-copy'
 
         # Brightness
-        bindsym XF86MonBrightnessDown exec 'light -U 10'
-        bindsym XF86MonBrightnessUp exec 'light -A 10'
+        bindsym XF86MonBrightnessDown exec 'brightnessctl set 5%-'
+        bindsym XF86MonBrightnessUp exec 'brightnessctl set +5%'
 
         # Volume
         bindsym XF86AudioRaiseVolume exec 'pactl set-sink-volume @DEFAULT_SINK@ +1%'
