f0c5a8a45fd4b03fb8022ad82f4fba07cc8846fb
Love Lysell Berglund <lovistovis0@gmail.com>
2025-11-10 21:29:40 +0100

    remove extra portal

--- hosts/nixbox-hp/base.nix
+++ hosts/nixbox-hp/base.nix
@@ -111,18 +111,5 @@
     };
   };
 
-  xdg.portal = {
-    enable = true;
-    xdgOpenUsePortal = true;
-    config = {
-      common.default = [ "gtk" ];
-      hyprland.default = [ "gtk" "hyprland" ];
-    };
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-hyprland
-      xdg-desktop-portal-gtk
-    ];
-  };
-
   time.hardwareClockInLocalTime = true;
 }
