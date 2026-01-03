8dbdaff4d6983669fa25e086a7c000c8c929ade2
Love Lysell Berglund <lovistovis0@gmail.com>
2026-01-03 15:17:57 +0100

    direnv hide_env_diff

--- home.nix
+++ home.nix
@@ -47,6 +47,7 @@ in {
       enable = true;
       enableZshIntegration = true;
       nix-direnv.enable = true;
+      config.global.hide_env_diff = true;
     };
     hyprlock.enable = true;
     tofi = {
