e0b982ebf4da2da6fe5442d1fb17532c29f129de
Love Lysell Berglund <love.lysell.berglund@gmail.com>
2026-09-01 09:06:22 +0200

    feat: hyprland cachix

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -16,6 +16,11 @@ in
     my-sddm-astronaut
   ];
 
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
   programs = {
     hyprland = {
       enable = true;
