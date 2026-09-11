7d2687cea55fb19eef0a3d5e83968b0914d7dc10
Love Lysell Berglund <love.lysell.berglund@gmail.com>
2026-09-11 13:13:35 +0200

    lock

--- flake.nix
+++ flake.nix
@@ -79,7 +79,6 @@
         mkHost "nixbox-hp" [
           ./modules/nixos/wayland.nix
           ./modules/nixos/hyprland.nix
-          # ./modules/nixos/kde-plasma.nix
           ./modules/nixos/nix-ld.nix
         ] [
           ./modules/home-manager/hyprland.nix
